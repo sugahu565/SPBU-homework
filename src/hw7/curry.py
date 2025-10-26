9ed3ddfc864d67dbf869504d6995960bb734461f
Katya Tupikova <k_tupikova00@mail.ru>
2025-10-26 23:40:19 +0300

    Add curry

---
+++ src/hw7/curry.py
@@ -0,0 +1,20 @@
+def __curry(function, length_arg, args):
+    if length_arg < len(args):
+        raise Exception("too many arguments")
+    if length_arg == len(args):
+        try:
+            return function(*args)
+        except:
+            raise Exception("invalid arguments or invalid number of arguments")
+
+    def next_function(x):
+        return __curry(function, length_arg, args + [x])
+
+    return next_function
+
+
+def curry(function, lenght_arg):
+    if lenght_arg < 0:
+        raise Exception("invalid lenght of arguments")
+    return __curry(function, lenght_arg, [])
+
