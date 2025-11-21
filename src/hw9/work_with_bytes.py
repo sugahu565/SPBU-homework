16e6da57105c55bd98eb469db0a82b4db350e702
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-21 17:19:04 +0300

    add functions that working with bytes

--- src/hw9/work_with_bytes.py
+++ src/hw9/work_with_bytes.py
@@ -9,9 +9,10 @@ def byte_to_bit(msg) -> str:
 def bit_to_byte(msg) -> bytes:
     #class <str> -> class <bytes>
     byte_array = bytearray()
-    for i in range(8, len(msg), 8):
-        byte_array.append(int(msg[i - 8:i]))
-    lost_symb = len(msg) - len(msg) % 8
-    if lost_symb != 0:
-        byte_array.append(int(msg[lost_symb:].ljust(8, "0")))
+    need_len = len(msg) // 8 * 8
+    for i in range(0, need_len, 8):
+        byte_array.append(int(msg[i:i + 8], 2))
+    lost_symb = msg[need_len:]
+    if len(lost_symb) > 0:
+        byte_array.append(int(lost_symb.ljust(8, "0"), 2))
     return byte_array
