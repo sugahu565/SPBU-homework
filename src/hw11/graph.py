1f7b73142d58f1e5e61e35b4aeae1ef91656e6b7
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-22 00:16:52 +0300

    add class dfs

---
+++ src/hw11/graph.py
@@ -0,0 +1,39 @@
+class Graph:
+    def __init__(self, edges: dict[int, list[int]]):
+        self.number_vertex = len(edges)
+        self.edges = edges
+        self.ind = 0
+        self.order = []
+
+
+    def get_dfs_order(self):
+        order_dfs = []
+        visited = set()
+
+
+        def dfs(now):
+            visited.add(now)
+            order_dfs.append(now)
+            for to in self.edges[now]:
+                if to not in visited:
+                    dfs(to)
+
+
+        for x in self.edges:
+            dfs(x)
+
+        return order_dfs
+
+
+    def __iter__(self):
+        self.order = self.get_dfs_order()
+        self.ind = 0
+        return self
+
+
+    def __next__(self):
+        if self.ind < self.number_vertex:
+            r = self.order[self.ind]
+            self.ind += 1
+            return r
+        raise StopIteration
